132118cd3f6dabd0fd35549344dc1a3d36513008
dkwasny@kent.edu <dkwasny@kent.edu@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2009-05-30 02:02:24 +0000

    Added initial files.  So far the program simply parses a chart file and outputs the contents to the console.  Will start the graphical part soon.

---
+++ trunk/Test_Chart_Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chart_View
+{
+    class Test_Chart_Class
+    {
+        public static void test()
+        {
+            Chart test = new Chart("chart.chart");
+            test.print_info();
+            Console.WriteLine("SUCCESS!! Press enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
