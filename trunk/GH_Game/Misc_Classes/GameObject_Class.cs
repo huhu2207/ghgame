9a60556fcfcb205da689069fcc55fdf41f30c015
dkwasny@kent.edu <dkwasny@kent.edu@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2009-09-24 07:11:05 +0000

    Lets hope this works...

---
+++ trunk/GH_Game/Misc_Classes/GameObject_Class.cs
@@ -0,0 +1,31 @@
+﻿// The classes and functions for the various game objects used
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Chart_View
+{
+    class gameObject
+    {
+        //Constructor
+        public gameObject(Texture2D loadedTex, Rectangle spritePos)
+        {
+            rotation = 0;
+            position = Vector2.Zero;
+            sprite = loadedTex;
+            center = new Vector2(sprite.Width / 2, sprite.Height / 2);
+            velocity = Vector2.Zero;
+            alive = false;
+            spriteSheetPosition = spritePos;
+        }
+
+        //Public data members
+        public Texture2D sprite;
+        public Vector2 position;
+        public float rotation;
+        public Vector2 center;
+        public Vector2 velocity;
+        public bool alive;
+        public Rectangle spriteSheetPosition;
+    }
+}
---
+++ trunk/GH_Game/Misc_Classes/Timer_Class.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Chart_View
+{
+    // Timer class that counts via miliseconds
+    class Timer
+    {
+        // Default constructor
+        public Timer()
+        {
+            timer_name = "default";
+            current_time = 0;
+            end_value = 1;
+            is_up = false;
+        }
+
+        // Typical constructor
+        public Timer(string input_name)
+        {
+            timer_name = input_name;
+            current_time = 0;
+            end_value = 1;
+            is_up = false;
+        }
+
+        // Updates the counter
+        public void Update(double in_time)
+        {
+            current_time += in_time;
+
+            // If the timer passes the end value, set the "is_up" value to true
+            if (current_time >= end_value)
+                is_up = true;
+        }
+
+        public string timer_name;  // The name/description of the timer
+        public double current_time;  // The current value of the timer
+        public double end_value;  // When the timer is considered "done"
+        public bool is_up;  // Shows if the timer is done counting
+    }
+}
