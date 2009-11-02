7a4368095db5aefca830d05e6adaecdd11f9fd0c
dkwasny <dkwasny@kent.edu>
2009-11-02 01:13:11 +0000

    problems

---
+++ GH_Game/Chartt/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GH_Game.Chart
+{
+    // Base abstract class for Note and Event
+    class Entity
+    {
+        public uint Location;
+        public uint TickValue;
+    }
+}
