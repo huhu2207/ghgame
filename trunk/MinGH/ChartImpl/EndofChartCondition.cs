45cfff6e81d2daa4371a75aa390e3255d64fe2c2
dkwasny@kent.edu <dkwasny@kent.edu@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2009-11-02 22:15:05 +0000

    Changed how I check for the end of the chart when adding the time values of each event/note and created a class to make life easier.  The song now stops when the current time value passes the last note/event's time value.

---
+++ trunk/MinGH/ChartImpl/EndofChartCondition.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinGH.ChartImpl
+{
+    // This class allows me to put the numerous conditions for finding the end of a chart into
+    // one easy to edit place.
+    class EndofChartCondition
+    {
+        public bool noMoreEvents;
+        public bool noMoreGreenNotes;
+        public bool noMoreRedNotes;
+        public bool noMoreYellowNotes;
+        public bool noMoreBlueNotes;
+        public bool noMoreOrangeNotes;
+        public bool noMoreSPNotes;
+
+        public EndofChartCondition()
+        {
+            noMoreEvents = false;
+            noMoreGreenNotes = false;
+            noMoreRedNotes = false;
+            noMoreYellowNotes = false;
+            noMoreBlueNotes = false;
+            noMoreOrangeNotes = false;
+            noMoreSPNotes = false;
+        }
+
+        // This object is false (aka done with) when all its members are true
+        public static bool operator true(EndofChartCondition myCondition)
+        {
+            if ((myCondition.noMoreEvents == true) &&
+                (myCondition.noMoreGreenNotes == true) &&
+                (myCondition.noMoreRedNotes == true) &&
+                (myCondition.noMoreYellowNotes == true) &&
+                (myCondition.noMoreBlueNotes == true) &&
+                (myCondition.noMoreOrangeNotes == true) &&
+                (myCondition.noMoreSPNotes == true))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        // This object is false (aka still active) when any of its members are false
+        public static bool operator false(EndofChartCondition myCondition)
+        {
+            if ((myCondition.noMoreEvents == true) &&
+                (myCondition.noMoreGreenNotes == true) &&
+                (myCondition.noMoreRedNotes == true) &&
+                (myCondition.noMoreYellowNotes == true) &&
+                (myCondition.noMoreBlueNotes == true) &&
+                (myCondition.noMoreOrangeNotes == true) &&
+                (myCondition.noMoreSPNotes == true))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
