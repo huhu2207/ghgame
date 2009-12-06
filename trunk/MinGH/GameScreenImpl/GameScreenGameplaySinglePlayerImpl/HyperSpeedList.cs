48999226321354d99be5390a461bfc1ae33982d5
dkwasny@kent.edu <dkwasny@kent.edu@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2009-12-06 00:30:35 +0000

    Made a cheesy way for variable note speed.  Simply used precomputed numbers (e.g. hand tested).  The actual equation is escaping me at the moment

---
+++ trunk/MinGH/GameScreenImpl/GameScreenGameplaySinglePlayerImpl/HyperSpeedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinGH.GameScreenImpl.GameScreenGameplaySinglePlayerImpl
+{
+    /// <summary>
+    /// A class containing a list of predefined hyperspeed values that worked on my system.
+    /// This is a horrible approach, but I can't think up a proper function to find these
+    /// values.
+    /// </summary>
+    public class HyperSpeedList
+    {
+        public HyperSpeedList()
+        {
+            theList.Add(new HyperSpeedValue(963, 0.5));
+            theList.Add(new HyperSpeedValue(820, 0.6));
+            theList.Add(new HyperSpeedValue(705, 0.7));
+            theList.Add(new HyperSpeedValue(615, 0.8));
+            theList.Add(new HyperSpeedValue(560, 0.9));
+            theList.Add(new HyperSpeedValue(500, 1.0));
+        }
+        public List<HyperSpeedValue> theList = new List<HyperSpeedValue>();
+    }
+}
