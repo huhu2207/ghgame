c853eaa268e97c3d6a833fb9e38c3521959fd572
dkwasny@kent.edu <dkwasny@kent.edu@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2010-01-08 23:16:22 +0000

    Started work on a SongSelectionScreen.  Changed the ChangeGameState function to take a GameStateEnum as opposed to flat integer codes.  Modified the KeyboardInputManager to wait two updates before taking any actions since the the current and previous stats within the class do not contain any real information until two updates pass by.

---
+++ trunk/MinGH/Enum/GameStateEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinGH.Enum
+{
+    public enum GameStateEnum
+    {
+        MainMenu,
+        Options,
+        SongSelection,
+        SinglePlayer,
+        QuitGame
+    }
+}
--- trunk/MinGH/GameScreen/KeyboardInputManager.cs
+++ trunk/MinGH/GameScreen/KeyboardInputManager.cs
@@ -7,16 +7,28 @@ namespace MinGH.GameScreen
     {
         KeyboardState currentState, previousState;
 
+        /// <summary>
+        /// The manager needs to wait two frames before the keyboard states will
+        /// be accurate (i.e. the currentState and previousState need to be filled
+        /// out with real information).
+        /// </summary>
+        private int updatesWaited;
+
         public KeyboardInputManager()
         {
             currentState = new KeyboardState();
             previousState = new KeyboardState();
+            updatesWaited = 0;
         }
 
         public void processKeyboardState(KeyboardState inputState)
         {
             previousState = currentState;
             currentState = inputState;
+            if (updatesWaited < 2)
+            {
+                updatesWaited++;
+            }
         }
 
         public KeyboardState getCurrentState()
@@ -26,18 +38,39 @@ namespace MinGH.GameScreen
 
         public bool keyIsHit(Keys key)
         {
-            return ((currentState.IsKeyDown(key) && previousState.IsKeyUp(key)));
+            if (updatesWaited >= 2)
+            {
+                return ((currentState.IsKeyDown(key) && previousState.IsKeyUp(key)));
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool keyIsHeld(Keys key)
         {
-            // Check is the key was either already held down, or just hit
-            return ((currentState.IsKeyDown(key) && previousState.IsKeyDown(key)) || keyIsHit(key));
+            if (updatesWaited >= 2)
+            {
+                // Check is the key was either already held down, or just hit
+                return ((currentState.IsKeyDown(key) && previousState.IsKeyDown(key)) || keyIsHit(key));
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool keyWasHeld(Keys key)
         {
-            return (currentState.IsKeyUp(key) && previousState.IsKeyDown(key));
+            if (updatesWaited <= 2)
+            {
+                return (currentState.IsKeyUp(key) && previousState.IsKeyDown(key));
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public Keys getHighestHeldKey()
