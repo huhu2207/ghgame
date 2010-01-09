3c9380838e1420c5fcbcdaeaaa0cab8d8e6cc3a9
dkwasny@kent.edu <dkwasny@kent.edu@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2010-01-09 01:53:43 +0000

    Renamed the Misc_Classes namespace to MiscClasses.  I need to think of a better place for these...

---
+++ trunk/MinGH/MiscClasses/GameObject.cs
@@ -0,0 +1,98 @@
+﻿// The classes and functions for the various game objects used
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MinGH.MiscClasses
+{
+    /// <remarks>
+    /// The wrapper class that allows for easy creation and manipulation of visible
+    /// objects on the game screen.  Note this class has functionality for sprite sheets.
+    /// </remarks>
+    public class GameObject
+    {
+        /// <summary>
+        /// Creats a game object from a texture, rectangle and offset.
+        /// </summary>
+        /// <param name="loadedTex">
+        /// The whole texture or sprite sheet this object will use.
+        /// </param>
+        /// <param name="spriteRect">
+        /// The rectangle in which the first sprite in the sheet is located.
+        /// Set this to the size of the texture if sprite sheets are not being used.
+        /// </param>
+        /// <param name="offset">
+        /// The padding on the left side of the spritesheet.  Use this if all the
+        /// sprites appear to be pushed to the right slightly.
+        /// </param>
+        public GameObject(Texture2D loadedTex, Rectangle spriteRect, int offset)
+        {
+            rotation = 0;
+            position = Vector2.Zero;
+            spriteSheet = loadedTex;
+            velocity = Vector2.Zero;
+            alive = false;
+            spriteSheetRectangle = spriteRect;
+            center = new Vector2(spriteRect.Width / 2, spriteRect.Height / 2);
+            spriteSheetOffset = offset;
+        }
+
+		/// <summary>
+		/// Gets the center location of the sprite at its current location on the screen.
+		/// Simply gets the position (top left of the sprite rectangle) and adds the
+		/// distance to the center of the sprite rectangle to it.
+		/// </summary>
+		/// <returns>
+		/// The center position of the sprite.
+		/// </returns>
+        public Vector2 getCenterPosition()
+        {
+            return new Vector2(position.X + center.X, position.Y + center.Y);
+        }
+
+        /// <summary>
+        /// The physical texture this object will use.  This can be a sprite or a
+        /// spritesheet.
+        /// </summary>
+        public Texture2D spriteSheet;
+		
+		/// <summary>
+		/// The position this game object is currently located.  This position is where
+		/// the top left of the sprite is to be drawn.
+		/// </summary>
+        public Vector2 position;
+		
+		/// <summary>
+		/// The rotation the sprite currently has.
+		/// </summary>
+        public float rotation;
+		
+		/// <summary>
+		/// The center point of the sprite texture used.  This is really calculated using
+		/// the provided sprite sheet rectangle.
+		/// </summary>
+        public Vector2 center;
+		
+		/// <summary>
+		/// The speed and direction in which the sprite is to be currently moving
+		/// </summary>
+        public Vector2 velocity;
+		
+		/// <summary>
+		/// A boolean value that dictates wether this sprite is to be drawn or not.
+		/// </summary>
+        public bool alive;
+		
+		/// <summary>
+		/// The rectangle that encompasses the desire section of the sprite sheet that
+		/// this object will currently use.  If it is set to the size of the sprite sheet
+		/// this will operate as a normal texture.
+		/// </summary>
+        public Rectangle spriteSheetRectangle;
+		
+		/// <summary>
+		/// Informs the program that there is a padding on the left side of the sprite sheet.
+		/// Use this if all the sprites appear to be slightly moved to the right.
+		/// </summary>
+        public int spriteSheetOffset;
+    }
+}
---
+++ trunk/MinGH/MiscClasses/Timer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace MinGH.MiscClasses
+{
+    /// <remarks>
+    /// A basic timer class.
+    /// </remarks>
+    class Timer
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Timer()
+        {
+            timer_name = "default";
+            current_time = 0;
+            end_value = 1;
+            is_up = false;
+        }
+
+        /// <summary>
+        /// A constructor that initializes the timer's name
+        /// </summary>
+        /// <param name="input_name">
+        /// The name for the new timer.
+        /// </param>
+        public Timer(string input_name)
+        {
+            timer_name = input_name;
+            current_time = 0;
+            end_value = 1;
+            is_up = false;
+        }
+
+        /// <summary>
+        /// Updates the counter according to the input value.
+        /// </summary>
+        /// <param name="in_time">
+        /// The value to be added to the current time.
+        /// </param>
+        public void Update(double in_time)
+        {
+            current_time += in_time;
+
+            // If the timer passes the end value, set the "is_up" value to true
+            if (current_time >= end_value)
+                is_up = true;
+        }
+
+		/// <summary>
+		/// The name/description of the timer.
+		/// </summary>
+        public string timer_name;
+		
+		/// <summary>
+		/// The current value of the timer.
+		/// </summary>
+        public double current_time;
+		
+		/// <summary>
+		/// The upper bound to the timer.
+		/// </summary>
+        public double end_value;
+		
+		/// <summary>
+		/// Tells the programmer if the timer is up.
+		/// </summary>
+        public bool is_up;  
+    }
+}
