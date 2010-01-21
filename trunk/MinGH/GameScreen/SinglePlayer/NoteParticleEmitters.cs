b548c6ad6505c19200e331761e8cc00139c9d36a
dkwasny@kent.edu <dkwasny@kent.edu@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2010-01-21 22:03:19 +0000

    Changed the emitters a little.  Forgot to commit the scale change for the game object.

--- trunk/MinGH/GameScreen/SinglePlayer/NoteParticleEmitters.cs
+++ trunk/MinGH/GameScreen/SinglePlayer/NoteParticleEmitters.cs
@@ -91,7 +91,7 @@ namespace MinGH.GameScreen.SinglePlayer
             lineEmitterToAdd.ReleaseScale = new VariableFloat { Anchor = 50f, Variation = 0.5f };
             lineEmitterToAdd.ReleaseSpeed = new VariableFloat { Anchor = 200f, Variation = 1f };
             lineEmitterToAdd.ReleaseOpacity = new VariableFloat { Anchor = 0.2f, Variation = 0f };
-            lineEmitterToAdd.Term = 0.8f;
+            lineEmitterToAdd.Term = 0.3f;
             lineEmitterToAdd.ParticleTextureAssetName = "Particles\\FlowerBurst";
             lineEmitterToAdd.ReleaseColour = Color.Orange.ToVector3();
             lineEmitterToAdd.Length = (themeSetting.laneBorderSize * 4) + (themeSetting.laneSize * 4);
