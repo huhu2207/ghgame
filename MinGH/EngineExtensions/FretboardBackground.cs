513f51ff39f61420bac0c2b7744ff000b59a754b
dkwasny <dkwasny@kent.edu>
2010-09-27 07:01:05 +0000

    Cleaning up some classes.

--- MinGH/EngineExtensions/FretboardBackground.cs
+++ MinGH/EngineExtensions/FretboardBackground.cs
@@ -18,51 +18,5 @@ namespace MinGH.EngineExtensions
             spriteSheetStepX = 0;
             spriteSheetStepY = 0;
         }
-
-        /// <summary>
-        /// The position of this fretboard in 3D space.
-        /// </summary>
-        //public override Vector3 position3D
-        //{
-        //    get
-        //    {
-        //        return _position3D;
-        //    }
-
-        //    set
-        //    {
-        //        _position3D = value;
-        //        vertices[0].Position = _position3D;
-        //        vertices[1].Position = _position3D + new Vector3(spriteSheet.Width, 0, 0) * new Vector3(_scale3D.X, 1, 1);
-        //        vertices[2].Position = _position3D + new Vector3(0, 0, spriteSheet.Height) * new Vector3(1, 1, _scale3D.Z);
-        //        vertices[3].Position = _position3D + new Vector3(spriteSheet.Width, 0, spriteSheet.Height) * _scale3D;
-        //        vertices[4].Position = _position3D + new Vector3(0, 0, spriteSheet.Height) * new Vector3(1, 1, _scale3D.Z);
-        //        vertices[5].Position = _position3D + new Vector3(spriteSheet.Width, 0, 0) * new Vector3(_scale3D.X, 1, 1);
-        //    }
-        //}
-
-        /// <summary>
-        /// The scaling value of this fretboard.
-        /// </summary>
-        //public override Vector3 scale3D
-        //{
-        //    get
-        //    {
-        //        return _scale3D;
-        //    }
-        //    set
-        //    {
-        //        _scale3D = value;
-        //        float newWidth = pixelsPerSpriteSheetStepX * value.X;
-        //        float newHeight = pixelsPerSpriteSheetStepY * value.Z;
-
-        //        vertices[0].Position = _position3D + new Vector3(0, 0, -newHeight);
-        //        vertices[1].Position = _position3D + new Vector3(newWidth, 0, 0);
-        //        vertices[2].Position = _position3D;
-        //        vertices[3].Position = _position3D + new Vector3(newWidth, 0, 0);
-        //        vertices[4].Position = _position3D + new Vector3(0, 0, -newHeight);
-        //        vertices[5].Position = _position3D + new Vector3(newWidth, 0, -newHeight);
-        //    }
-        //}
     }
 }
