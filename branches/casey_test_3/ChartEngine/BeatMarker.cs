27060022dafa47f46ce78ee525eb29b826c471ae
dkwasny67@gmail.com <dkwasny67@gmail.com@c90eff4e-4cbc-11de-aa6a-4703752c21e7>
2012-08-06 20:37:50 +0000

    creating casey_test_3

---
+++ branches/casey_test_3/ChartEngine/BeatMarker.cs
@@ -0,0 +1,26 @@
+﻿casey_test_2
+using System;
+
+namespace ChartEngine
+{
+    public class Beatmarker
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Beatmarker()
+        {
+            timeValue = 0;
+            markerDuration = 4;
+        }
+
+        public Beatmarker(uint inTimeValue, int inDuration)
+        {
+            timeValue = inTimeValue;
+            markerDuration = inDuration;
+        }
+
+        public uint timeValue { get; set; }
+        public int markerDuration { get; set; }  // "4" = Quarter note, "8" = Eighth note, etc.
+    }
+}
